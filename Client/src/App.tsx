0f0cd4fd997d00da9980918f8fe8e967977eb33b
TranThang2004 <134066904+TranThang2004@users.noreply.github.com>
2024-08-02 04:51:58 +0700

    sửa category

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -5,10 +5,11 @@ import Guarantee from './components/Guarantee'
 import Header from './components/Header'
 import ProductCard from './components/ProductCard'
 import ProductSection from './components/ProductSection'
-import Slidebar from './components/Slidebar'
+
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Category from './pages/category';
+import Slidebar from './components/SlideBar'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Header />
       <Slidebar/>
       <CategoryNavBar/>
+      <Category />q
       <ProductSection/>
       <ProductCard/>
       <Guarantee/>
